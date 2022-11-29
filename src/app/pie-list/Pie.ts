5c2f9689cb854605b8cb9ae1b0618764d16d665e
Luciana Y. Romero <lu.yael.r@gmail.com>
2022-11-28 21:59:58 -0300

    Agrega interfaz y template del componente pie-list

---
+++ src/app/pie-list/Pie.ts
@@ -0,0 +1,10 @@
+import { ComponentFactoryResolver } from "@angular/core"
+
+export interface Pie{
+    name: String,
+    description: String,
+    price: number,
+    stock: number,
+    image: String,
+    clearance: Boolean,
+}
--- src/app/pie-list/pie-list.component.ts
+++ src/app/pie-list/pie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pie } from './pie';
 
 @Component({
   selector: 'app-pie-list',
@@ -6,13 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./pie-list.component.scss']
 })
 export class PieListComponent {
-pie ={
-  "name":"Lemon Pie",
-  "description":"Base de galletitas de vainilla con manteca, crema de limón y merengue italiano.",
-  "price": 600,
-  "stock": 12,
-  "image":"assets/images/lemonPie.jpg"
-}
+cackes: Pie [] =[
+ {
+  name:"Lemon Pie",
+  description:"Base de galletitas de vainilla con manteca, crema de limón y merengue italiano.",
+  price: 600,
+  stock: 12,
+  image:"assets/images/lemonPie.jpg",
+  clearance: false,
+}, 
+{
+name:"Torta Óreo",
+description:"Base de galletitas Oreo con manteca, exquisita mousse de dulce de leche y trozos de chocolate semi amargo.",
+price: 700,
+stock: 8,
+image:"assets/images/oreo.jpg",
+clearance: false,
+}, 
+
+{
+  name:"Torta de manzana",
+  description:"Frola de vainilla y manzanas verdes, con una exquisita cobertura de strudel de almendras.",
+  price: 550,
+  stock: 5,
+  image:"assets/images/manzana.jpg",
+  clearance: true,
+  },
+  {
+  name:"Torta tipo Balcarce",
+  description:"  Pionono de vainilla, crema, dulce de leche natural, merengue y salsa de frambuesa.",
+  price: 750,
+  stock: 0,
+  image:"assets/images/balcarce.jpg",
+  clearance: false,
+  },
+
+];
+
 constructor(){}
 ngOnInit(): void {
 
